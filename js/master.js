cd5305316dae744bd80fd1693bab3ea61264ef8e
Angelo Lamacchia <angelo.lamacchia@live.it>
2021-02-18 15:31:20 +0100

    inizio esercizio

---
+++ js/master.js
@@ -0,0 +1,14 @@
+// const anno = 2021;
+
+// var annoNascita = prompt('In che anno sei nato?')
+
+
+// document.getElementById('my_password').innerHTML=('Hai '+(anno - annoNascita)+' anni');
+
+var Nome = prompt('Mi diresti cortesemente il tuo nome?');
+
+var Cognome = prompt ('So che ti rompo ma vorrei anche il cognome');
+
+var ColorePref = prompt ('Ultima cosa il tuo colore preferito?');
+
+document.getElementById('my_password').innerHTML = (Nome+Cognome+ColorePref+'19');
