a634ee14a31294ee5bb6b2648e5072cd1e34d3ee
Angelo Lamacchia <angelo.lamacchia@live.it>
2021-02-18 16:46:06 +0100

    piccole modifiche

--- js/master.js
+++ js/master.js
@@ -1,14 +1,17 @@
+// costante anno in cui siamo
 const anno = 2021;
 
 // aggiunge un numero random tra 0 e 99
-numRandom = Math.round(Math.random()*100);
-
-var Nome = prompt('Mi diresti cortesemente il tuo nome?');
-
+var numRandom = Math.round(Math.random()*100);
+//variabile nome
+var nome = prompt('Mi diresti cortesemente il tuo nome?');
+//variabile anno di nascita
 var annoNascita = prompt('In che anno sei nato?')
+//variabile cognome
+var cognome = prompt ('So che ti rompo ma vorrei anche il cognome');
+//variabile colore preferito
+var colorePref = prompt ('Ultima cosa il tuo colore preferito?');
 
-var Cognome = prompt ('So che ti rompo ma vorrei anche il cognome');
-
-var ColorePref = prompt ('Ultima cosa il tuo colore preferito?');
-
-document.getElementById('my_password').innerHTML = (Nome+Cognome+ColorePref+(anno - annoNascita)+numRandom);
+//risultato visibile in my_password
+document.getElementById('my_password').innerHTML =
+(nome+cognome+colorePref+ (anno - parseInt(annoNascita)) +numRandom);
