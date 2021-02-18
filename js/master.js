f940f76ceabb9188fc55548fcfc7a11514a4fa90
Angelo Lamacchia <angelo.lamacchia@live.it>
2021-02-18 16:16:20 +0100

    aggiunta età e numero random

--- js/master.js
+++ js/master.js
@@ -1,14 +1,14 @@
-// const anno = 2021;
+const anno = 2021;
 
-// var annoNascita = prompt('In che anno sei nato?')
-
-
-// document.getElementById('my_password').innerHTML=('Hai '+(anno - annoNascita)+' anni');
+// aggiunge un numero random tra 0 e 99
+numRandom = Math.round(Math.random()*100);
 
 var Nome = prompt('Mi diresti cortesemente il tuo nome?');
+
+var annoNascita = prompt('In che anno sei nato?')
 
 var Cognome = prompt ('So che ti rompo ma vorrei anche il cognome');
 
 var ColorePref = prompt ('Ultima cosa il tuo colore preferito?');
 
-document.getElementById('my_password').innerHTML = (Nome+Cognome+ColorePref+'19');
+document.getElementById('my_password').innerHTML = (Nome+Cognome+ColorePref+(anno - annoNascita)+numRandom);
